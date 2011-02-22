0848225cbc4fb65070fda725c7b65c5e0c959f8c
inghamn <inghamn@4e447b6a-6056-7046-2609-0fe1680c5616>
2011-02-22 19:52:36 +0000

    The homepage sends you to login automatically
    git-svn-id: https://civic-crm.googlecode.com/svn/trunk@39 4e447b6a-6056-7046-2609-0fe1680c5616

--- html/home.php
+++ html/home.php
@@ -1,8 +1,12 @@
 <?php
 /**
- * @copyright 2006-2008 City of Bloomington, Indiana
+ * @copyright 2011 City of Bloomington, Indiana
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
+if (!isset($_SESSION['USER'])) {
+	header('Location: '.BASE_URL.'/login');
+	exit();
+}
 $template = new Template();
 echo $template->render();
