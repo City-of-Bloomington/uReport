3669da84e95b0a231f691197f283dc3c3970607d
wsibo12 <wsibo12@4e447b6a-6056-7046-2609-0fe1680c5616>
2011-03-03 21:40:22 +0000

    Modified ticket migration file
    git-svn-id: https://civic-crm.googlecode.com/svn/trunk@76 4e447b6a-6056-7046-2609-0fe1680c5616

--- scripts/migration/calltrack/3_tickets.php
+++ scripts/migration/calltrack/3_tickets.php
@@ -30,182 +30,192 @@ $result = $pdo->query($sql);
 $results = $result->fetchAll(PDO::FETCH_ASSOC);
 foreach($results as $row){
   //
-	// Import the Dates
-	$ticket = new Ticket();
-	if ($row['date']) {
-		$ticket->setEnteredDate($row['date']);
-	}
-	// Import the Person
-	if (isset($row['username']) && $row['username']) {
-	  try {
-		$user = new User(strtolower($row['username']));
-		$ticket->setEnteredByPerson($user->getPerson());
-		if(isset($row['referralId']) && $row['referralId']){
-		  $ticket->setReferredPerson($user->getPerson());
-		}
-		else {
-		  $ticket->setAssignedPerson($user->getPerson());
-		}
+  // Import the Dates
+  $ticket = new Ticket();
+  if ($row['date']) {
+	$ticket->setEnteredDate($row['date']);
+  }
+  // Import the Person
+  if (isset($row['username']) && $row['username']) {
+	try {
+	  $user = new User(strtolower($row['username']));
+	  $ticket->setEnteredByPerson($user->getPerson());
+	  if(isset($row['referralId']) && $row['referralId']){
+		$ticket->setReferredPerson($user->getPerson());
 	  }
-	  catch (Exception $e) {
+	  else {
+		$ticket->setAssignedPerson($user->getPerson());
 	  }
 	}
-	// status
-	switch ($row['status']) {
-	case 'Requested':
-	case 'Referred':			  
-	  $ticket->setStatus('open');
+	catch (Exception $e) {
+	}
+  }
+  // status
+  switch ($row['status']) {
+  case 'Requested':
+  case 'Referred':			  
+	$ticket->setStatus('open');
+	break;
+  default:
+	$ticket->setStatus('closed');
+  }
+  // if the request has resolution record ==> it is closed
+  if(isset($row['resolutionId']) && $row['resolutionId']){
+	$ticket->setStatus('closed');
+	$ticket->setResolution(new Resolution('Resolved'));
+  }
+  // No address or location info
+  $ticket->save();
+
+  // Create the issue on this ticket
+  $issue = new Issue();
+  $issue->setDate($ticket->getEnteredDate());
+  $issue->setTicket($ticket);
+  if ($ticket->getEnteredByPerson()) {
+	$issue->setEnteredByPerson($ticket->getEnteredByPerson());
+  }
+  $issue->setNotes($row['notes']);
+  if($row['contactMethod']){
+	switch ($row['contactMethod']) {
+	case 'phone':
+	  $issue->setContactMethod('Phone Call');
 	  break;
-	default:
-	  $ticket->setStatus('closed');
-	}
-    // if the request has resolution record ==> it is closed
-    if(isset($row['resolutionId']) && $row['resolutionId']){
-	  $ticket->setStatus('closed');
-	  $ticket->setResolution(new Resolution('Resolved'));
-    }
-	// No address or location info
-	$ticket->save();
-
-	// Create the issue on this ticket
-	$issue = new Issue();
-	$issue->setDate($ticket->getEnteredDate());
-	$issue->setTicket($ticket);
-	if ($ticket->getEnteredByPerson()) {
-		$issue->setPerson($ticket->getEnteredByPerson());
-	}
-	$issue->setNotes($row['notes']);
-	if($row['contactMethod']){
-	  switch ($row['contactMethod']) {
-	  case 'phone':
-		$issue->setContactMethod('Phone Call');
-		break;
-	  case 'email':			  
-		$issue->setContactMethod('Email');
+	case 'email':			  
+	  $issue->setContactMethod('Email');
 	  break;
-	  case 'letter':			  
-		$issue->setContactMethod('Letter');
+	case 'letter':			  
+	  $issue->setContactMethod('Letter');
 	  break;	  
-	  case 'in-person':			  
-		$issue->setContactMethod('Walk In');
+	case 'in-person':			  
+	  $issue->setContactMethod('Walk In');
 	  break;	  
-	  default:  // fax
-		$issue->setContactMethod('Other');
-	  }
+	default:  // fax
+	  $issue->setContactMethod('Other');
 	}
-	if (preg_match('/COMPLAINT/',$row['notes'])) {
-		$issue->setIssueType('Complaint');
-	}
-	if (preg_match('/VIOLATION/',$row['notes'])) {
-		$issue->setIssueType('Violation');
-	}
-	else {
-		$issue->setIssueType('Request');
-	}
-
-	$personList = new
-	  PersonList(array(
-					   'firstname'=>$row['firstname'],
-					   'lastname'=>$row['lastname'],
-					   'email'=>$row['email'],
-					   'phone'=>$row['phone']
-					   ));
-	if (count($personList)) {
-	  $issue->setConstituent($personList[0]);
-	}
-	$issue->save();
-
-	/**
-	 * Create the Ticket History
-	 *
-	 * We're going to run through the workflow of a ticket.
-	 * To help us out, we'll want to keep track of the last person who worked
-	 * on the ticket at each step of the workflow
-	 */
-	$lastPerson = null;
-	if ($ticket->getEnteredByPerson()) {
-		$lastPerson = $ticket->getEnteredByPerson();
-	}
-	$history = new TicketHistory();
-	$history->setEventLabel('opened');
-	$history->setEnteredDate($ticket->getEnteredDate());
-	$history->setEventDate($ticket->getEnteredDate());
-	$history->setTicket($ticket);
-	if ($lastPerson) {
-		$history->setPerson($lastPerson);
-		$history->setEventDescription("Ticket opened by {$lastPerson->getFullname()}");
-	}
-	else {
-		$history->setEventDescription('Ticket opened');
-	}
-	$history->setNotes('Ticket opened');
-	$history->save();
+  }
+  if (preg_match('/COMPLAINT/',$row['notes'])) {
+	$issue->setIssueType('Complaint');
+  }
+  if (preg_match('/VIOLATION/',$row['notes'])) {
+	$issue->setIssueType('Violation');
+  }
+  else {
+	$issue->setIssueType('Request');
+  }
+  
+  $personList = new
+	PersonList(array(
+					 'firstname'=>$row['firstname'],
+					 'lastname'=>$row['lastname'],
+					 'email'=>$row['email'],
+					 'phone'=>$row['phone']
+					 ));
+  if (count($personList)) {
+	$issue->setReportedByPerson($personList[0]);
+  }
+  $issue->save();
 	
-	//
-	// looking for resolutions,responses related to this request
-	// these will be considered as actions
-	//
-	$sql = "select date,notes,person
+  /**
+   * Create the Ticket History
+   *
+   * We're going to run through the workflow of a ticket.
+   * To help us out, we'll want to keep track of the last person who worked
+   * on the ticket at each step of the workflow
+   */
+  $lastPerson = null;
+  if ($ticket->getEnteredByPerson()) {
+	$lastPerson = $ticket->getEnteredByPerson();
+  }
+  $history = new TicketHistory();
+  $history->setEventLabel('opened');
+  $history->setEnteredDate($ticket->getEnteredDate());
+  $history->setEventDate($ticket->getEnteredDate());
+  $history->setTicket($ticket);
+  if ($lastPerson) {
+	$history->setPerson($lastPerson);
+	$history->setEventDescription("Ticket opened by {$lastPerson->getFullname()}");
+  }
+  else {
+	$history->setEventDescription('Ticket opened');
+  }
+  $history->setNotes('Ticket opened');
+  $history->save();
+  
+  //
+  // looking for resolutions,responses related to this request
+  // these will be considered as actions
+  //
+  $sql = "select date,notes,person
 		from referrals 
 		where request_id=".$row['id'];
-	//
-	$result2 = $pdo->query($sql);
-	while ($row2 = $result2->fetch(PDO::FETCH_ASSOC)) {
-	  $history = new TicketHistory();
-	  $history->setEventLabel('referral');
-	  $history->setEnteredDate($row2['date']);
-	  $history->setEventDate($row2['date']);
-	  $history->setTicket($ticket);
-	  $history->setEventDescription('This ticket was referred');	  
-	  $history->setNotes($row2['notes']);
-	  $person = new Person();
-	  $person->setFirstname($row2['person']);
-	  $person->save();
-	  $history->setPerson($person);
-	  $history->save();
+  //
+  $result2 = $pdo->query($sql);
+  while ($row2 = $result2->fetch(PDO::FETCH_ASSOC)) {
+	$history = new TicketHistory();
+	$history->setEventLabel('referral');
+	$history->setEnteredDate($row2['date']);
+	$history->setEventDate($row2['date']);
+	$history->setTicket($ticket);
+	$history->setEventDescription('This ticket was referred');	  
+	$history->setNotes($row2['notes']);
+	list($firstname,$lastname) = explode(' ',$row2['person']);
+	$personList = new PersonList(array(
+									   'firstname'=>$firstname,
+									   'lastname'=>$lastname));
+	if (count($personList)) {
+	  $history->setPerson($personList[0]);
 	}
-	//
-	// start with resolutions
-	//
-	$sql = "select date,notes,username
+	else{
+	  // if no match found, we put all in first name
+	  $person = new Person();
+	  $person->setFirstname($row2['person']); 
+	  $person->save();
+	  $history->setPerson($person);		
+	}
+	$history->save();
+  }
+  //
+  // start with resolutions
+  //
+  $sql = "select date,notes,username
 		from resolutions r
         left join users u on r.user_id = u.id 
 		where r.request_id=".$row['id'];
-	$result2 = $pdo->query($sql);
-	while ($row2 = $result2->fetch(PDO::FETCH_ASSOC)) {
-	  $history = new TicketHistory();
-	  $history->setEventLabel('resolution');
-	  $history->setEnteredDate($row2['date']);
-	  $history->setEventDate($row2['date']);
-	  $history->setTicket($ticket);
-	  $history->setEventDescription('This ticket was resolved');
-	  $history->setNotes($row['notes']);
-	  $user = new User(strtolower($row2['username']));
-	  $history->setPerson($user->getPerson());
-	  $history->save();
-	}
-	//
-	// responses go in issueHistory
-	//
-	$sql = "select r.date as date,r.notes as notes,
+  $result2 = $pdo->query($sql);
+  while ($row2 = $result2->fetch(PDO::FETCH_ASSOC)) {
+	$history = new TicketHistory();
+	$history->setEventLabel('resolution');
+	$history->setEnteredDate($row2['date']);
+	$history->setEventDate($row2['date']);
+	$history->setTicket($ticket);
+	$history->setEventDescription('This ticket was resolved');
+	$history->setNotes($row['notes']);
+	$user = new User(strtolower($row2['username']));
+	$history->setPerson($user->getPerson());
+	$history->save();
+  }
+  //
+  // responses go in issueHistory
+  //
+  $sql = "select r.date as date,r.notes as notes,
         u.username as username, c.name as contactMethod
 		from responses r
         left join referrals rf on r.referral_id = rf.id
         left join users u on r.user_id = u.id
         left join contactMethods c on r.contactMethod_id = c.id
 		where rf.request_id=".$row['id'];
-	$result2 = $pdo->query($sql);
-	while ($result2 && $row2 = $result2->fetch(PDO::FETCH_ASSOC)) {
-	  $history = new IssueHistory();
-	  $history->setEventLabel('response');
-	  $history->setEnteredDate($row2['date']);
-	  $history->setEventDate($row2['date']);
-	  $history->setIssue($issue);
-	  $history->setEventDescription('Response to this issue by '.$row2['contactMethod']);
-	  $history->setNotes($row2['notes']);	  
-	  $user = new User(strtolower($row2['username']));
-	  $history->setPerson($user->getPerson());
-	  $history->save();
-	  // print_r($history);
-	}
+  $result2 = $pdo->query($sql);
+  while ($result2 && $row2 = $result2->fetch(PDO::FETCH_ASSOC)) {
+	$history = new IssueHistory();
+	$history->setEventLabel('response');
+	$history->setEnteredDate($row2['date']);
+	$history->setEventDate($row2['date']);
+	$history->setIssue($issue);
+	$history->setEventDescription('Response to this issue by '.$row2['contactMethod']);
+	$history->setNotes($row2['notes']);	  
+	$user = new User(strtolower($row2['username']));
+	$history->setPerson($user->getPerson());
+	$history->save();
+	// print_r($history);
+  }
 }
