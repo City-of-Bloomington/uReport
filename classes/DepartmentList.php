cbd06055720ed40f2d19eb2515881cc93e95da94
inghamn <inghamn@4e447b6a-6056-7046-2609-0fe1680c5616>
2011-04-19 18:05:38 +0000

    DepartmentList now uses Mongo
    git-svn-id: https://civic-crm.googlecode.com/svn/branches/mongo@205 4e447b6a-6056-7046-2609-0fe1680c5616

--- classes/DepartmentList.php
+++ classes/DepartmentList.php
@@ -2,72 +2,52 @@
 /**
  * A collection class for Department objects
  *
- * This class creates a zend_db select statement.
- * ZendDbResultIterator handles iterating and paginating those results.
- * As the results are iterated over, ZendDbResultIterator will pass each desired
- * row back to this class's loadResult() which will be responsible for hydrating
- * each Department object
- *
- * Beyond the basic $fields handled, you will need to write your own handling
- * of whatever extra $fields you need
- *
  * @copyright 2011 City of Bloomington, Indiana
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
-class DepartmentList extends ZendDbResultIterator
+class DepartmentList extends MongoResultIterator
 {
 	/**
-	 * Creates a basic select statement for the collection.
-	 *
-	 * Populates the collection if you pass in $fields
-	 * Setting itemsPerPage turns on pagination mode
-	 * In pagination mode, this will only load the results for one page
-	 *
 	 * @param array $fields
-	 * @param int $itemsPerPage Turns on Pagination
-	 * @param int $currentPage
 	 */
-	public function __construct($fields=null,$itemsPerPage=null,$currentPage=null)
+	public function __construct($fields=null)
 	{
-		parent::__construct($itemsPerPage,$currentPage);
+		parent::__construct();
 		if (is_array($fields)) {
 			$this->find($fields);
 		}
 	}
 
 	/**
-	 * Populates the collection
+	 * Populates the collection, using strict matching of the requested fields
 	 *
 	 * @param array $fields
-	 * @param string|array $order Multi-column sort should be given as an array
+	 * @param array $order
 	 * @param int $limit
-	 * @param string|array $groupBy Multi-column group by should be given as an array
 	 */
-	public function find($fields=null,$order='id',$limit=null,$groupBy=null)
+	public function find($fields=null,$order=null,$limit=null)
 	{
-		$this->select->from('departments');
-
-		// Finding on fields from the departments table is handled here
+		$search = array();
 		if (count($fields)) {
 			foreach ($fields as $key=>$value) {
-				$this->select->where("$key=?",$value);
+				if ($value) {
+					$search[$key] = (string)$value;
+				}
 			}
 		}
-
-		// Finding on fields from other tables requires joining those tables.
-		// You can handle fields from other tables by adding the joins here
-		// If you add more joins you probably want to make sure that the
-		// above foreach only handles fields from the departments table.
-
-		$this->select->order($order);
+		if (count($search)) {
+			$this->cursor = $this->mongo->departments->find($search);
+		}
+		else {
+			$this->cursor = $this->mongo->departments->find();
+		}
+		if ($order) {
+			$this->cursor->sort($order);
+		}
 		if ($limit) {
-			$this->select->limit($limit);
+			$this->cursor->limit($limit);
 		}
-		if ($groupBy) {
-			$this->select->group($groupBy);
-		}
-		$this->populateList();
 	}
 
 	/**
@@ -79,8 +59,8 @@ class DepartmentList extends ZendDbResultIterator
 	 * @param int $key The index of the result row to load
 	 * @return Department
 	 */
-	protected function loadResult($key)
+	protected function loadResult($data)
 	{
-		return new Department($this->result[$key]);
+		return new Department($data);
 	}
 }
