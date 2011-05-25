1d6d4c73313d0b238749fe04727fd4026074fdb9
inghamn <inghamn@4e447b6a-6056-7046-2609-0fe1680c5616>
2011-05-25 13:58:22 +0000

    Lcoation find form was not preserving any return_urls
    git-svn-id: https://civic-crm.googlecode.com/svn/trunk@306 4e447b6a-6056-7046-2609-0fe1680c5616

--- html/locations/home.php
+++ html/locations/home.php
@@ -3,13 +3,18 @@
  * @copyright 2011 City of Bloomington, Indiana
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @param GET return_url (optional)
  */
 $template = isset($_GET['format'])
 	? new Template('default',$_GET['format'])
 	: new Template('locations');
 
 if ($template->outputFormat=='html') {
-	$template->blocks['location-panel'][] = new Block('locations/findLocationForm.inc');
+	$findForm = new Block('locations/findLocationForm.inc');
+	if (isset($_GET['return_url'])) {
+		$findForm->return_url = $_GET['return_url'];
+	}
+	$template->blocks['location-panel'][] = $findForm;
 }
 
 if (isset($_GET['location'])) {
