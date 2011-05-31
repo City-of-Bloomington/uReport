a6e551fd3aff7f6eb79041d1d23c2ffb861f1973
inghamn <inghamn@4e447b6a-6056-7046-2609-0fe1680c5616>
2011-05-31 20:09:55 +0000

    You can now delete people who don't have any tickets Fixes issue 58
    git-svn-id: https://civic-crm.googlecode.com/svn/trunk@327 4e447b6a-6056-7046-2609-0fe1680c5616

--- classes/Person.php
+++ classes/Person.php
@@ -82,6 +82,18 @@ class Person extends MongoRecord
 		$mongo->people->save($this->data,array('safe'=>true));
 	}
 
+	public function delete()
+	{
+		if ($this->getId()) {
+			if ($this->hasTickets()) {
+				throw new Exception('people/personStillHasTickets');
+			}
+
+			$mongo = Database::getConnection();
+			$mongo->people->remove(array('_id'=>$this->getId()));
+		}
+	}
+
 	//----------------------------------------------------------------
 	// Generic Getters
 	//----------------------------------------------------------------
@@ -478,14 +490,29 @@ class Person extends MongoRecord
 	}
 
 	/**
-	 * Transfers all data from a person, then deletes that person
+	 * Returns true if this person's ID is associated with any fields in the ticket records
 	 *
-	 * This person will end up containing all information from both people
-	 *
-	 * @param Person $person
+	 * @return boolean
 	 */
-	public function mergeFrom(Person $person)
+	public function hasTickets()
 	{
-
+		if ($this->getId()) {
+			$mongo = Database::getConnection();
+			$tickets = $mongo->tickets->findOne(array(
+				'$or'=>array(
+					array('enteredByPerson._id'=>new MongoId($this->data['_id'])),
+					array('assignedPerson._id'=>new MongoId($this->data['_id'])),
+					array('referredPerson._id'=>new MongoId($this->data['_id'])),
+					array('issues.enteredByPerson._id'=>new MongoId($this->data['_id'])),
+					array('issues.reportedByPerson._id'=>new MongoId($this->data['_id'])),
+					array('issues.media.person._id'=>new MongoId($this->data['_id'])),
+					array('history.enteredByPerson._id'=>new MongoId($this->data['_id'])),
+					array('history.actionPerson._id'=>new MongoId($this->data['_id']))
+				)
+			));
+			if ($tickets) {
+				return true;
+			}
+		}
 	}
 }
---
+++ html/people/deletePerson.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright 2011 City of Bloomington, Indiana
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @param GET person_id
+ */
+if (userIsAllowed('People')) {
+	try {
+		$person = new Person($_GET['person_id']);
+		$person->delete();
+	}
+	catch (Exception $e) {
+		$_SESSION['errorMessages'][] = $e;
+	}
+}
+else {
+	$_SESSION['errorMessages'][] = new Exception('noAccessAllowed');
+}
+
+header('Location: '.BASE_URL.'/people');
