26139fd64219cfdb29198d7783f7aaedd025892a
inghamn <inghamn@4e447b6a-6056-7046-2609-0fe1680c5616>
2011-06-17 13:29:26 +0000

    Added drag and drop of tickets to the merge form. Fixes issue 88
    git-svn-id: https://civic-crm.googlecode.com/svn/trunk@393 4e447b6a-6056-7046-2609-0fe1680c5616

---
+++ html/js/tickets/merge.js
@@ -0,0 +1,18 @@
+"use strict";
+YUI().use('dd-delegate','dd-drop-plugin', function(Y) {
+	var del = new Y.DD.Delegate({
+		cont: '#location-panel .ticketList',
+		nodes: 'tbody tr'
+	});
+	var ticketA = new Y.DD.Drop({
+		node:'#ticket_id_a'
+	});
+	var ticketB = new Y.DD.Drop({
+		node:'#ticket_id_b'
+	});
+
+	Y.DD.DDM.on('drop:hit',function(e) {
+		var matches = e.drag.get('node').one('.button a').get('href').match(/ticket_id=(.+)$/);
+		e.drop.get('node').set('value',matches[1]);
+	});
+});
