7128690d72205aafa9836d50d7bb854c3daa498c
inghamn <inghamn@4e447b6a-6056-7046-2609-0fe1680c5616>
2011-06-28 18:19:13 +0000

    Disabled the javascript location chooser for now.  It's still missing way to set the location data in the ticket form.  Also, adding all the listeners to the a href's is very slow and locks up the browser
    git-svn-id: https://civic-crm.googlecode.com/svn/trunk@469 4e447b6a-6056-7046-2609-0fe1680c5616

--- html/js/tickets/chooseLocation.js
+++ html/js/tickets/chooseLocation.js
@@ -1,36 +1,36 @@
 "use strict";
 // Activate the Find Address Form as an overlay
-YUI().use('node','overlay','io-form',function(Y) {
+YUI().use('node', 'overlay', 'io-form', function (Y) {
 	var overlay = new Y.Overlay({
-		srcNode:'#find_location_overlay',
-		footerContent:'<span class="button"><span class="cancel"><button type="button">Cancel</button</div></div>',
-		align:{
-			node:'#findAddressButton',
-			points:[Y.WidgetPositionAlign.TL, Y.WidgetPositionAlign.BL]
+		srcNode: '#find_location_overlay',
+		footerContent: '<span class="button"><span class="cancel"><button type="button">Cancel</button</div></div>',
+		align: {
+			node: '#findAddressButton',
+			points: [Y.WidgetPositionAlign.TL, Y.WidgetPositionAlign.BL]
 		}
 	});
-	Y.io(BASE_URL+'/locations/partial.php?partial=locations/findLocationForm.inc;return_url='+BASE_URL+'/locations/viewLocation.php',{
-		on:{
-			complete:function(id,o,args) {
-				overlay.set('bodyContent',o.responseText);
+	Y.io(BASE_URL + '/locations/partial.php?partial=locations/findLocationForm.inc;return_url=' + BASE_URL + '/locations/viewLocation.php', {
+		on: {
+			complete: function (id, o, args) {
+				overlay.set('bodyContent', o.responseText);
 			}
 		}
 	});
 	overlay.render();
 	overlay.hide();
 
-	Y.on('click',function(e) {
+	Y.on('click', function (e) {
 		e.preventDefault();
 		overlay.show();
-	},'#findAddressButton');
+	}, '#findAddressButton');
 
-	Y.on('click',Y.bind(overlay.hide, overlay),'#find_location_overlay .cancel button');
-	Y.on('submit',function(e) {
+	Y.on('click', Y.bind(overlay.hide, overlay), '#find_location_overlay .cancel button');
+	Y.on('submit', function (e) {
 		e.preventDefault();
-		Y.io(BASE_URL+'/locations/partial.php?partial=locations/findLocationResults.inc',{
-			form: { id:e.target },
+		Y.io(BASE_URL + '/locations/partial.php?partial=locations/findLocationResults.inc', {
+			form: { id: e.target },
 			on: {
-				complete: function(id,o,args) {
+				complete: function (id, o, args) {
 					var results = Y.one('#find_location_overlay .findLocationResults');
 					if (results) {
 						results.remove(true);
@@ -40,12 +40,12 @@ YUI().use('node','overlay','io-form',function(Y) {
 						o.responseText,
 						Y.WidgetStdMod.AFTER
 					);
-					Y.all('#find_location_overlay .findLocationResults a').on('click',function(e) {
+					Y.all('#find_location_overlay .findLocationResults a').on('click', function (e) {
 						e.preventDefault();
-						var uri = e.target.get('href')+';partial=location-panel;disableLinks';
-						Y.io(uri,{
+						var uri = e.target.get('href') + ';partial=location-panel;disableLinks';
+						Y.io(uri, {
 							on: {
-								complete: function(id,o,args) {
+								complete: function (id, o, args) {
 									var locationInfo = Y.one('#location-panel .locationInfo'),
 										ticketList = Y.one('#location-panel .ticketList');
 									if (locationInfo) { locationInfo.remove(true); }
