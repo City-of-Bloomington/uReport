cb2201be238956713d585ab752e67c5a9fc5e820
inghamn <inghamn@4e447b6a-6056-7046-2609-0fe1680c5616>
2011-06-30 15:51:20 +0000

    Added support for CAS authentication.  If configured, it is now the default authentication system
    git-svn-id: https://civic-crm.googlecode.com/svn/trunk@475 4e447b6a-6056-7046-2609-0fe1680c5616

--- html/login/home.php
+++ html/login/home.php
@@ -1,11 +1,45 @@
 <?php
 /**
- * @copyright 2009-2010 City of Bloomington, Indiana
+ * @copyright 2011 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
 $return_url = isset($_REQUEST['return_url']) ? $_REQUEST['return_url'] : BASE_URL;
 
-$template = new Template();
-$template->blocks[] = new Block('loginForm.inc',array('return_url'=>$return_url));
-echo $template->render();
+// If they don't have CAS configured, send them onto the application's
+// internal authentication system
+if (!defined('CAS')) {
+	header('Location: '.BASE_URL.'/login.php?return_url='.$return_url);
+	exit();
+}
+
+$_SESSION['return_url'] = $return_url;
+
+require_once CAS.'/SimpleCAS/Autoload.php';
+$options = array('hostname'=>CAS_SERVER,'uri'=>CAS_URI);
+$protocol = new SimpleCAS_Protocol_Version2($options);
+$client = SimpleCAS::client($protocol);
+$client->forceAuthentication();
+
+if ($client->isAuthenticated()) {
+	try {
+		$_SESSION['USER'] = new Person($client->getUsername());
+
+		if (isset($_SESSION['return_url'])) {
+			$return_url = $_SESSION['return_url'];
+			unset($_SESSION['return_url']);
+
+			header('Location: '.$return_url);
+			exit();
+		}
+		else {
+			header('Location: '.BASE_URL);
+			exit();
+		}
+	}
+	catch (Exception $e) {
+		$_SESSION['errorMessages'][] = $e;
+	}
+}
+unset($_SESSION['return_url']);
+header('Location: '.BASE_URL);
--- html/login/login.php
+++ html/login/login.php
@@ -8,25 +8,33 @@
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
-try {
-	$person = new Person($_POST['username']);
-	if ($person->authenticate($_POST['password'])) {
-		$_SESSION['USER'] = $person;
+$return_url = isset($_REQUEST['return_url']) ? $_REQUEST['return_url'] : BASE_URL;
+
+if (isset($_POST['username'])) {
+	try {
+		$person = new Person($_POST['username']);
+		if ($person->authenticate($_POST['password'])) {
+			$_SESSION['USER'] = $person;
+		}
+		else {
+			throw new Exception('wrongPassword');
+		}
+	}
+	catch (Exception $e) {
+		$_SESSION['errorMessages'][] = $e;
+		header('Location: '.BASE_URL);
+		exit();
+	}
+
+	// The user has successfully logged in.  Redirect them wherever you like
+	if ($_POST['return_url']) {
+		header('Location: '.$_POST['return_url']);
 	}
 	else {
-		throw new Exception('wrongPassword');
+		header('Location: '.BASE_URL.'/tickets');
 	}
 }
-catch (Exception $e) {
-	$_SESSION['errorMessages'][] = $e;
-	header('Location: '.BASE_URL);
-	exit();
-}
 
-// The user has successfully logged in.  Redirect them wherever you like
-if ($_POST['return_url']) {
-	header('Location: '.$_POST['return_url']);
-}
-else {
-	header('Location: '.BASE_URL.'/tickets');
-}
+$template = new Template();
+$template->blocks[] = new Block('loginForm.inc',array('return_url'=>$return_url));
+echo $template->render();
