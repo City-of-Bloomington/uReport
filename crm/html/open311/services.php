77aad3601d975d313956d1c82ba081d65ac339e7
inghamn <inghamn@4e447b6a-6056-7046-2609-0fe1680c5616>
2011-09-09 15:51:30 +0000

    Open311 Service calls now filtered by user authentication
    git-svn-id: https://civic-crm.googlecode.com/svn/trunk@568 4e447b6a-6056-7046-2609-0fe1680c5616

--- crm/html/open311/services.php
+++ crm/html/open311/services.php
@@ -4,6 +4,8 @@
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
+$person = isset($_SESSION['USER']) ? $_SESSION['USER'] : 'anonymous';
+
 // Grab the format from the file extension used in the url
 $format = preg_match("/[^.]+$/",$_SERVER['REQUEST_URI'],$matches)
 	? strtolower($matches[0])
@@ -16,10 +18,15 @@ preg_match('|/open311/v2/services/?([0-9a-f]{24})?.*|',$_SERVER['REQUEST_URI'],$
 if (isset($matches[1]) && $matches[1]) {
 	try {
 		$category = new Category($matches[1]);
-		$template->blocks[] = new Block('open311/serviceInfo.inc',array('category'=>$category));
+		if ($category->allowsDisplay($person)) {
+			$template->blocks[] = new Block('open311/serviceInfo.inc',array('category'=>$category));
+		}
+		else {
+			// Not allowed to see tickets for this category
+		}
 	}
 	catch (Exception $e) {
-		# Unknown service
+		// Unknown service
 	}
 }
 // Just display  the full service list
