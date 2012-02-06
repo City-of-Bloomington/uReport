fcef486132b67af878ad4fd1e429b24ece667cb8
inghamn <inghamn@4e447b6a-6056-7046-2609-0fe1680c5616>
2012-02-06 15:02:19 +0000

    Search form is now an accordian with checkboxes
    git-svn-id: https://civic-crm.googlecode.com/svn/trunk@710 4e447b6a-6056-7046-2609-0fe1680c5616

--- crm/public/js/tickets/search.js
+++ crm/public/js/tickets/search.js
@@ -1,24 +1,18 @@
 "use strict";
 YUI().use('node', function(Y) {
-	var fieldset = Y.one('#advanced-search');
-	var select = Y.Node.create('<select><option></option></select>');
-	var label;
+	var fields = Y.all('#advanced-search .multiselect');
+	fields.each(function (node) {
+		node.addClass('collapsed');
+	});
 
-
-	fieldset.all('tr').each( function() {
-		label = this.one('label');
-		select.append(
-			'<option value="' + label.getAttribute('for') + '">' + label.getContent() + '</option>'
-		);
-		if (!this.one('#'+label.getAttribute('for')).get('value')) {
-			this.addClass('hidden');
+	function toggle(e) {
+		if (this.hasClass('collapsed')) {
+			this.replaceClass('collapsed','expanded');
 		}
-	});
+		else {
+			this.replaceClass('expanded','collapsed');
+		}
+	}
 
-	select.on('change',function (e) {
-		fieldset.one('#'+e.currentTarget.get('value')).get('parentNode.parentNode').removeClass('hidden');
-		this.set('selectedIndex',-1);
-	});
-
-	fieldset.append(select);
+	Y.delegate('click', toggle, '#advanced-search', '.multiselect');
 });
