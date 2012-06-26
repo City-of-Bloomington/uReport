eccc9a7476f692cb30653b8671ac6b9662baf4bf
inghamn <inghamn@4e447b6a-6056-7046-2609-0fe1680c5616>
2012-06-26 19:33:50 +0000

    Reports now have a new tabular format
    git-svn-id: https://civic-crm.googlecode.com/svn/branches/mysql@927 4e447b6a-6056-7046-2609-0fe1680c5616

--- crm/models/Report.php
+++ crm/models/Report.php
@@ -11,84 +11,92 @@ class Report
 
 	public static function assignments($get)
 	{
+		$where = self::handleSearchParameters($get);
+		$sql = "select t.assignedPerson_id, t.status, t.category_id,
+					p.firstname, p.lastname,
+					c.name as category,
+					t.resolution_id, r.name as resolution,
+					count(*) as count
+				from tickets t
+				     join people        p on t.assignedPerson_id=p.id
+				left join categories    c on t.category_id=c.id
+				left join resolutions   r on t.resolution_id=r.id
+				left join ticketHistory h on t.id=h.ticket_id and h.action_id=7
+				$where
+				group by t.assignedPerson_id, t.status, t.category_id,
+					p.firstname, p.lastname,
+					c.name,
+					t.resolution_id, r.name
+				order by t.assignedPerson_id, t.category_id, t.status, t.resolution_id";
+
 		self::$zend_db = Database::getConnection();
-		self::$select = self::$zend_db->select();
-		self::$select->from(array('t'=>'tickets'), array(
-			't.assignedPerson_id', 't.status', 't.category_id',
-			'count'=>'count(*)',
-			'seconds'=>'avg(time_to_sec(timediff(ifnull(h.actionDate, now()), t.enteredDate)))'
-		));
-		self::$select->join(
-			array('p'=>'people'),
-			't.assignedPerson_id=p.id',
-			array('p.firstname','p.lastname')
-		);
-		self::$select->joinLeft(array('c'=>'categories'), 't.category_id=c.id', array('c.name'));
-		self::$select->joinLeft(array('h'=>'ticketHistory'), 't.id=h.ticket_id and h.action_id=7', array());
-		self::$select->group(array(
-			't.assignedPerson_id', 't.status', 't.category_id',
-			'p.firstname', 'p.lastname',
-			'c.name',
-		));
-
-		self::handleSearchParameters($get);
-
-		$result = self::$zend_db->fetchAll(self::$select);
-		$o = array();
-		foreach ($result as $row) {
-			$id = $row['assignedPerson_id'];
-			$cid = $row['category_id'] ? $row['category_id'] : 0;
-
-			$o[$id]['person']['firstname'] = $row['firstname'];
-			$o[$id]['person']['lastname']  = $row['lastname'];
-			$o[$id]['categories'][$cid]['name'] = $row['name'];
-			$o[$id]['categories'][$cid][$row['status']] = array('count'=>$row['count'], 'seconds'=>$row['seconds']);
+		$result = self::$zend_db->fetchAll($sql);
+		$d = array();
+		foreach ($result as $r) {
+			$pid = $r['assignedPerson_id'];
+			$cid = $r['category_id'];
+			if (!isset($d[$pid])) { $d[$pid]['name'] = "$r[firstname] $r[lastname]"; }
+			if (!isset($d[$pid]['categories'][$cid])) {
+				$d[$pid]['categories'][$cid] = array(
+					'name'=>$r['category'],
+					'data'=>array()
+				);
+			}
+			$d[$pid]['categories'][$cid]['data'][] = $r;
 		}
-		return $o;
+		return $d;
 	}
 
 	public static function categories($get)
 	{
+		$where = self::handleSearchParameters($get);
+		$sql = "select c.id as category_id, c.name as category,
+					t.assignedPerson_id, t.status, t.resolution_id,
+					p.firstname, p.lastname, r.name as resolution,
+					count(*) as count
+				from categories         c
+				     join tickets       t on c.id=t.category_id
+				     join people        p on t.assignedPerson_id=p.id
+				left join resolutions   r on t.resolution_id=r.id
+				left join ticketHistory h on t.id=h.ticket_id and h.action_id=7
+				$where
+				group by c.id, c.name,
+					t.assignedPerson_id, t.status, t.resolution_id,
+					p.firstname, p.lastname,
+					r.name
+				order by c.id, t.assignedPerson_id, t.status desc, t.resolution_id
+		";
 		self::$zend_db = Database::getConnection();
-		self::$select = self::$zend_db->select();
-		self::$select->from(array('c'=>'categories'), array('c.id', 'c.name'));
-		self::$select->join(array('t'=>'tickets'), 'c.id=t.category_id', array(
-			't.assignedPerson_id', 't.status',
-			'count'=>'count(*)',
-			'seconds'=>'avg(time_to_sec(timediff(ifnull(h.actionDate, now()), t.enteredDate)))'
-		));
-		self::$select->join(
-			array('p'=>'people'),
-			't.assignedPerson_id=p.id',
-			array('p.firstname','p.lastname')
-		);
-		self::$select->joinLeft(array('h'=>'ticketHistory'), 't.id=h.ticket_id and h.action_id=7', array());
-		self::$select->group(array(
-			'c.id', 'c.name',
-			't.assignedPerson_id', 't.status',
-			'p.firstname', 'p.lastname'
-		));
-
-		self::handleSearchParameters($get);
-		$result = self::$zend_db->fetchAll(self::$select);
-		$o = array();
-		foreach ($result as $row) {
-			$id = $row['id'];
-			$pid = $row['assignedPerson_id'];
-
-			$o[$id]['category']['name'] = $row['name'];
-			$o[$id]['people'][$pid]['firstname'] = $row['firstname'];
-			$o[$id]['people'][$pid]['lastname']  = $row['lastname'];
-			$o[$id]['people'][$pid][$row['status']] = array('count'=>$row['count'], 'seconds'=>$row['seconds']);
+		$d = array();
+		$result = self::$zend_db->fetchAll($sql);
+		foreach ($result as $r) {
+			$cid = $r['category_id'];
+			$pid = $r['assignedPerson_id'];
+			if (!isset($d[$cid])) { $d[$cid]['name'] = $r['category']; }
+			if (!isset($d[$cid]['people'][$pid])) {
+				$d[$cid]['people'][$pid] = array(
+					'name'=>"$r[firstname] $r[lastname]",
+					'data'=> array()
+				);
+			}
+			$d[$cid]['people'][$pid]['data'][] = $r;
 		}
-		return $o;
+		return $d;
 	}
 
 	/**
+	 * WARNING:
+	 * Be very careful here, we're handling SQL as raw strings for
+	 * both maintainability and performance reasons.
+	 * Make sure nothing from the $get array is used in a string.
+	 * Everything must be cleaned up before using in the where string
+	 *
 	 * @param array $get
+	 * @return string SQL for the where portion of a select
 	 */
 	private static function handleSearchParameters($get)
 	{
+		$options = array();
 		if (!empty($get['enteredDate'])) {
 			$start = !empty($get['enteredDate']['start'])
 				? date(ActiveRecord::MYSQL_DATE_FORMAT, strtotime($get['enteredDate']['start']))
@@ -96,20 +104,23 @@ class Report
 			$end = !empty($get['enteredDate']['end'])
 				? date(ActiveRecord::MYSQL_DATE_FORMAT, strtotime($get['enteredDate']['end']))
 				: date(ActiveRecord::MYSQL_DATE_FORMAT);
-			self::$select->where('t.enteredDate<=?', array($end));
-			self::$select->where('ifnull(h.actionDate, now())>=?', array($start));
+			$options[] = "(t.enteredDate<='$end' and ifnull(h.actionDate, now())>='$start')";
 		}
 		if (!empty($get['departments'])) {
 			$ids = array();
 			foreach (array_keys($get['departments']) as $i) { $ids[] = (int)$i; }
 			$ids = implode(',', $ids);
-			self::$select->where("p.department_id in ($ids)");
+			$options[] = "p.department_id in ($ids)";
 		}
 		if (!empty($get['categories'])) {
 			$ids = array();
 			foreach (array_keys($get['categories']) as $i) { $ids[] = (int)$i; }
 			$ids = implode(',', $ids);
-			self::$select->where("t.category_id in ($ids)");
+			$options[] = "t.category_id in ($ids)";
+		}
+		if ($options) {
+			$options = implode(' and ', $options);
+			return "where $options";
 		}
 	}
 }
