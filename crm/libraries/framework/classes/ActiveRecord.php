dea34a897339b889826f17b99ab9e70573dd08a1
inghamn <inghamn@4e447b6a-6056-7046-2609-0fe1680c5616>
2012-08-13 14:34:53 +0000

    We now try to parse dates using DATE_FORMAT first, only then falling back to strtotime(). Fixes issue 293
    git-svn-id: https://civic-crm.googlecode.com/svn/trunk@980 4e447b6a-6056-7046-2609-0fe1680c5616

--- crm/libraries/framework/classes/ActiveRecord.php
+++ crm/libraries/framework/classes/ActiveRecord.php
@@ -91,12 +91,10 @@ abstract class ActiveRecord
 	/**
 	 * Sets a date
 	 *
-	 * Dates should be in something strtotime() understands
+	 * Dates should be in DATE_FORMAT, set in configuration.inc
+	 * If we cannot parse the string using DATE_FORMAT, we will
+	 * fall back to trying something strtotime() understands
 	 * http://www.php.net/manual/en/function.strtotime.php
-	 *
-	 * If we cannot parse the date using strtotime formats,
-	 * we'll try to parse it according to the DATE_FORMAT.
-	 * DATE_FORMAT must be set in configuration.inc
 	 *
 	 * @param string $dateField
 	 * @param string $date
@@ -105,12 +103,12 @@ abstract class ActiveRecord
 	{
 		$date = trim($date);
 		if ($date) {
-			try {
-				$d = new DateTime($date);
-			}
-			catch (Exception $e) {
-				$d = DateTime::createFromFormat(DATE_FORMAT, $date);
-				if (!$d) {
+			$d = DateTime::createFromFormat(DATE_FORMAT, $date);
+			if (!$d) {
+				try {
+					$d = new DateTime($date);
+				}
+				catch (Exception $e) {
 					throw new Exception('unknownDateFormat');
 				}
 			}
