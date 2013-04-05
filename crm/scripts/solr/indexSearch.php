32ee54d57c0596208ff0a629e048c02f6a9d5681
inghamn <inghamn@bloomington.in.gov>
2013-04-05 09:40:49 -0400

    Indexing the Solr search index now automatically deletes the previous index.  You no longer have to manually delete the index on the Solr server.

--- crm/scripts/solr/indexSearch.php
+++ crm/scripts/solr/indexSearch.php
@@ -1,11 +1,13 @@
 <?php
 /**
- * @copyright 2012 City of Bloomington, Indiana
+ * @copyright 2012-2013 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
 include '../../configuration.inc';
 $search = new Search();
+$search->solrClient->deleteByQuery('*:*');
+$search->solrClient->commit();
 
 $sql = 'select * from tickets';
 $zend_db = Database::getConnection();
