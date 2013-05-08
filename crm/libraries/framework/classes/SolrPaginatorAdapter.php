9b132a9b127f3c6c35478f8326ce466f5a0fdbea
inghamn <inghamn@bloomington.in.gov>
2013-05-08 12:40:59 -0400

    Switched Solr client to solr-php-client.\nFixes #81

--- crm/libraries/framework/classes/SolrPaginatorAdapter.php
+++ crm/libraries/framework/classes/SolrPaginatorAdapter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2012 City of Bloomington, Indiana
+ * @copyright 2012-2013 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
@@ -8,7 +8,7 @@ class SolrPaginatorAdapter implements Zend_Paginator_Adapter_Interface
 {
 	private $solrObject;
 
-	public function __construct(SolrObject $solrObject)
+	public function __construct(Apache_Solr_Response $solrObject)
 	{
 		$this->solrObject = $solrObject;
 	}
