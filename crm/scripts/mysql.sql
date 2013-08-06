9fe89aca4b593b382a9a5e55917021bad947a80a
inghamn <inghamn@bloomington.in.gov>
2013-08-06 09:11:55 -0400

    Users can now save searches and reports. Fixes #16

--- crm/scripts/mysql.sql
+++ crm/scripts/mysql.sql
@@ -261,3 +261,12 @@ create table responses (
 	foreign key (contactMethod_id) references contactMethods(id),
 	foreign key (person_id)        references people        (id)
 );
+
+create table bookmarks (
+	id          int unsigned not null primary key auto_increment,
+	person_id   int unsigned not null,
+	`type`      varchar(128) not null default 'search',
+	name        varchar(128),
+	requestUri  varchar(255) not null,
+	foreign key (person_id) references people(id)
+);
