4344c0f7db03b5e872b182392893efa667472bf1
inghamn <inghamn@bloomington.in.gov>
2013-10-01 10:16:21 -0400

    Added a copy of generateTicketClusters.php to the migration directory.  Just as a reminder to run it.

---
+++ crm/scripts/migration/1.8.1-1.9/generateTicketClusters.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright 2013 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @author Quan Zhang <quanzhang@acm.org>
+ */
+include '../../configuration.inc';
+
+$zend_db = Database::getConnection();
+$zend_db->delete('ticket_geodata');
+$zend_db->query('truncate table geoclusters');
+
+$sql = "select id from tickets
+		where latitude is not null
+		  and longitude is not null";
+$results = $zend_db->fetchAll($sql);
+$c = count($results);
+foreach ($results as $i=>$row) {
+	$ticket = new Ticket($row['id']);
+	GeoCluster::updateTicketClusters($ticket);
+	echo "Ticket: $row[id] $i:$c\n";
+}
