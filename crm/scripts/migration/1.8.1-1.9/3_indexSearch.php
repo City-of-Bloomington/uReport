1572281122cafb40a3ba149f4133644a1e9db116
inghamn <inghamn@bloomington.in.gov>
2013-10-04 11:52:11 -0400

    Fixed migration script configuration path

--- crm/scripts/migration/1.8.1-1.9/3_indexSearch.php
+++ crm/scripts/migration/1.8.1-1.9/3_indexSearch.php
@@ -4,7 +4,7 @@
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
-include '../../configuration.inc';
+include '../../../configuration.inc';
 $search = new Search();
 $search->solrClient->deleteByQuery('*:*');
 $search->solrClient->commit();
