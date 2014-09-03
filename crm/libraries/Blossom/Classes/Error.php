a605474d08a526b7d548ea0693c8f3a8bb09944a
Cliff Ingham <inghamn@bloomington.in.gov>
2014-09-03 14:48:50 -0400

    Skidder Reporting now includes Fatal Errors.

--- crm/libraries/Blossom/Classes/Error.php
+++ crm/libraries/Blossom/Classes/Error.php
@@ -15,65 +15,23 @@ namespace Blossom\Classes;
 
 class Error
 {
+    /**
+     * Checks for a fatal error and calls our custom error handler
+     */
+    public static function shutdownHandler()
+    {
+        $e = error_get_last();
+        if ($e['type'] == E_ERROR) {
+            self::customExceptionHandler(new \ErrorException($e['message'], 0, $e['type'], $e['file'], $e['line']));
+        }
+    }
+
 	/**
 	* Provide nicely formatted error messages when PHP bombs out.
 	*/
 	public static function customErrorHandler($errno, $errstr, $errfile, $errline)
 	{
-		global $ERROR_REPORTING;
-
-		if (isset($ERROR_REPORTING)) {
-			if (in_array('PRETTY_PRINT',$ERROR_REPORTING)) {
-				echo "
-				<div id=\"errorMessages\">
-					<p><em>from ".ADMINISTRATOR_NAME.":</em>
-							There is an error in the code on this page that is through no fault of your own.
-							Errors of this sort need to be fixed immediately, though.
-							Please help us out by copying and pasting the following error message into an email and sending it to me at
-							<a href=\"mailto:".ADMINISTRATOR_EMAIL."\">".ADMINISTRATOR_EMAIL."</a>.
-					</p>
-					<p><strong>Code Error:</strong>  Error on line $errline of file $errfile:</p>
-					<p>$errstr</p>
-				</div>
-				";
-			}
-			if (in_array('EMAIL_ADMIN',$ERROR_REPORTING)) {
-				$subject = APPLICATION_NAME.' Error';
-				$message = "\t$_SERVER[REQUEST_URI]\n\nError on line $errline of file $errfile:\n$errstr\n\n";
-				$message.= print_r(debug_backtrace(),true);
-				mail(ADMINISTRATOR_EMAIL,$subject,$message,"From: apache@$_SERVER[SERVER_NAME]");
-			}
-
-			if (in_array('EMAIL_USER',$ERROR_REPORTING)
-					&& isset($_SESSION['USER'])
-					&& $_SESSION['USER']->getEmail()) {
-				$subject = APPLICATION_NAME.' Error';
-				$message = "\t$_SERVER[REQUEST_URI]\n\nError on line $errline of file $errfile:\n$errstr\n\n";
-				$message.= print_r(debug_backtrace(),true);
-				mail($_SESSION['USER']->getEmail(),
-					$subject,
-					$message,
-					"From: apache@$_SERVER[SERVER_NAME]");
-			}
-			if (in_array('SKIDDER',$ERROR_REPORTING)) {
-				$script = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : $_SERVER['SCRIPT_NAME'];
-				$message = "$script\nError on line $errline of file $errfile:\n$errstr\n";
-				$message.= print_r(debug_backtrace(),true);
-
-				$skidder = curl_init(SKIDDER_URL);
-				curl_setopt($skidder,CURLOPT_POST,true);
-				curl_setopt($skidder,CURLOPT_HEADER,true);
-				curl_setopt($skidder,CURLOPT_RETURNTRANSFER,true);
-				curl_setopt($skidder,CURLOPT_SSL_VERIFYPEER, false);
-				curl_setopt($skidder,
-							CURLOPT_POSTFIELDS,
-							array('application_id'=>SKIDDER_APPLICATION_ID,
-								'script'=>$script,
-								'type'=>$errstr,
-								'message'=>$message));
-				curl_exec($skidder);
-			}
-		}
+        self::customExceptionHandler(new \ErrorException($errstr, 0, $errno, $errfile, $errline));
 	}
 
 	/**
@@ -84,6 +42,8 @@ class Error
 		global $ERROR_REPORTING;
 
 		if (isset($ERROR_REPORTING)) {
+            echo "ERROR_REPORTING is configured\n";
+
 			if (in_array('PRETTY_PRINT',$ERROR_REPORTING)) {
 				echo "
 				<div id=\"errorMessages\">
@@ -93,46 +53,44 @@ class Error
 							Please help me out by copying and pasting the following error message into an email and sending it to me at
 							<a href=\"mailto:".ADMINISTRATOR_EMAIL."\">".ADMINISTRATOR_EMAIL."</a>.
 					</p>
-					<p><strong>Uncaught exception:</strong>
-						Exception on line {$exception->getLine()} of file {$exception->getFile()}:
+					<p><strong>Error:</strong>
+						on line {$exception->getLine()} of file {$exception->getFile()}:
 					</p>
 					<p>{$exception->getMessage()}</p>
 				</div>
 				";
 			}
-			if (in_array('EMAIL_ADMIN',$ERROR_REPORTING)) {
-				$subject = APPLICATION_NAME.' Exception';
-				$message = "\t$_SERVER[REQUEST_URI]\n\nException on line {$exception->getLine()} of file {$exception->getFile()}:\n{$exception->getMessage()}\n\n";
-				$message.= print_r(debug_backtrace(),true);
-				mail(ADMINISTRATOR_EMAIL,$subject,$message,"From: apache@$_SERVER[SERVER_NAME]");
-			}
-			if (in_array('EMAIL_USER',$ERROR_REPORTING)
-					&& isset($_SESSION['USER'])
-					&& $_SESSION['USER']->getEmail()) {
-				$subject = APPLICATION_NAME.' Exception';
-				$message = "\t$_SERVER[REQUEST_URI]\n\nException on line {$exception->getLine()} of file {$exception->getFile()}:\n{$exception->getMessage()}\n\n";
-				$message.= print_r(debug_backtrace(),true);
-				mail($_SESSION['USER']->getEmail(),
-					$subject,
-					$message,
-					"From: apache@$_SERVER[SERVER_NAME]");
-			}
-			if (in_array('SKIDDER',$ERROR_REPORTING)) {
-				$script = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : $_SERVER['SCRIPT_NAME'];
-				$message = "Error on line {$exception->getLine()} of file {$exception->getFile()}:\n{$exception->getMessage()}\n";
-				$message.= print_r(debug_backtrace(),true);
 
+            $script  = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : $_SERVER['SCRIPT_NAME'];
+            $server  = isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : 'localhost';
+
+            $subject = APPLICATION_NAME.' Error';
+            $message = "\t$script\n\nError on line {$exception->getLine()} of file {$exception->getFile()}:\n{$exception->getMessage()}\n\n";
+            $message.= print_r(debug_backtrace(), true);
+            $from    = "From: ".APPLICATION_NAME."@$server";
+
+            if (in_array('EMAIL_ADMIN', $ERROR_REPORTING)) {
+                mail(ADMINISTRATOR_EMAIL, $subject, $message, $from);
+            }
+            if (in_array('EMAIL_USER',$ERROR_REPORTING)
+                && isset($_SESSION['USER'])
+                && $_SESSION['USER']->getEmail()) {
+
+                mail($_SESSION['USER']->getEmail(), $subject, $message, $from);
+            }
+			if (in_array('SKIDDER', $ERROR_REPORTING)) {
+                echo "SKIDDER is configured\n";
 				$skidder = curl_init(SKIDDER_URL);
-				curl_setopt($skidder,CURLOPT_POST,true);
-				curl_setopt($skidder,CURLOPT_HEADER,true);
-				curl_setopt($skidder,CURLOPT_RETURNTRANSFER,true);
-				curl_setopt($skidder,CURLOPT_SSL_VERIFYPEER, false);
+				curl_setopt($skidder, CURLOPT_POST,           true);
+				curl_setopt($skidder, CURLOPT_HEADER,         true);
+				curl_setopt($skidder, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($skidder, CURLOPT_SSL_VERIFYPEER, false);
 				curl_setopt($skidder,
 							CURLOPT_POSTFIELDS,
-							array('application_id'=>SKIDDER_APPLICATION_ID,
-								'script'=>$script,
-								'type'=>'Uncaught Exception',
-								'message'=>$message));
+							['application_id' => SKIDDER_APPLICATION_ID,
+                             'script'         => $script,
+                             'type'           => $exception->getMessage(),
+                             'message'        => $message]);
 				curl_exec($skidder);
 			}
 		}
