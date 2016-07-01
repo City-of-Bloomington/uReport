9f2aa95078e170fea5005e7f408bab16f380eaee
inghamn <inghamn@bloomington.in.gov>
2016-07-01 10:42:12 -0400

    Added dropdown menu helper

---
+++ crm/templates/html/helpers/Dropdown.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @copyright 2016 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ */
+namespace Application\Templates\Helpers;
+
+use Blossom\Classes\Template;
+
+class Dropdown
+{
+	public function __construct(Template $template)
+	{
+		$this->template = $template;
+	}
+
+	public function dropdown(array $links, $title, $class=null)
+	{
+        $html = "
+        <nav class=\"dropdown $class\">
+            <button class=\"launcher\" aria-haspopup=\"true\" aria-expanded=\"false\">$title</button>
+            <div class=\"links\">
+                {$this->renderLinks($links)}
+            </div>
+        </nav>
+        ";
+        return $html;
+	}
+
+	private function renderLinks(array $links)
+	{
+        $html = '';
+        foreach ($links as $l) {
+            $html.= empty($l['subgroup'])
+                ? "<a href=\"$l[url]\">$l[label]</a>"
+                : "<div class=\"fn1-dropdown-subgroup\">{$this->renderLinks($l['subgroup'])}</div>";
+        }
+        return $html;
+	}
+}
