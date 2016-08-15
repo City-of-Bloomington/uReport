c45c04839f4a4cee35370e97159540de4c770ae8
inghamn <inghamn@bloomington.in.gov>
2016-08-15 11:51:27 -0400

    Location data moved inside of the marker infoWindow
    This cleans up the default view as well as the printout.
    
    Updates #227

--- crm/public/js/tickets/map.js
+++ crm/public/js/tickets/map.js
@@ -1,13 +1,14 @@
 google.maps.event.addDomListener(window, 'load', function() {
-	var map = new google.maps.Map(document.getElementById('location_map'), {
-		zoom:15,
-		center: new google.maps.LatLng(points[0].latitude, points[0].longitude),
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	});
-	for (var i in points) {
-		var marker = new google.maps.Marker({
-			position: new google.maps.LatLng(points[i].latitude, points[i].longitude),
-			map: map
-		});
-	}
+    var geodata   = document.querySelector('#locationInfo .geodata'),
+        latitude  = geodata .querySelector('dl.latitude  > dd').innerHTML,
+        longitude = geodata .querySelector('dl.longitude > dd').innerHTML,
+        map       = new google.maps.Map(document.getElementById('location_map'), {
+                        zoom:15,
+                        center: new google.maps.LatLng(latitude, longitude),
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                    }),
+        marker    = new google.maps.Marker({position:new google.maps.LatLng(latitude, longitude), map:map}),
+        info      = new google.maps.InfoWindow({content:geodata.innerHTML});
+
+    marker.addListener('click', function () { info.open(map, marker); });
 });
