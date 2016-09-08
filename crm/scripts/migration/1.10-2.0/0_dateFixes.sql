70e24246501331af278e020114bc331a7460871f
inghamn <inghamn@bloomington.in.gov>
2016-09-08 12:30:20 -0400

    Update database migration to account for null enteredDates
    Fixes #265

--- crm/scripts/migration/1.10-2.0/0_dateFixes.sql
+++ crm/scripts/migration/1.10-2.0/0_dateFixes.sql
@@ -1,5 +1,7 @@
 update ticketHistory set actionDate=enteredDate where actionDate=0;
 update issueHistory  set actionDate=enteredDate where actionDate=0;
+delete from ticketHistory where enteredDate='1970-01-01 00:00:00' and actionDate='1970-01-01 00:00:00';
+update ticketHistory set enteredDate=actionDate where enteredDate='1970-01-01 00:00:00';
 alter table tickets       modify enteredDate datetime /*!50700 not null default CURRENT_TIMESTAMP */;
 alter table ticketHistory modify actionDate  datetime /*!50700 not null default CURRENT_TIMESTAMP */;
 alter table issueHistory  modify actionDate  datetime /*!50700 not null default CURRENT_TIMESTAMP */;
