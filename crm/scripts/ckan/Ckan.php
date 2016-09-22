1c1ea46a16d7f59a1a00cdab01be0071af3faaad
inghamn <inghamn@bloomington.in.gov>
2016-09-22 15:08:20 -0400

    Created a script to export and upload data to CKAN resources
    Fixes #66

---
+++ crm/scripts/ckan/Ckan.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @copyright 2016 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ */
+namespace Application\Models;
+
+class Ckan
+{
+    private $config = [];
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+    function upload_resource($resource_id, $file)
+    {
+        echo "Uploading ".basename($file)."\n";
+
+        $fields = [
+            'id'     => $resource_id,
+            'upload' => new \CURLFile($file, 'text/csv', 'data.csv')
+        ];
+        $request = curl_init($this->config['ckan_url'].'/api/3/action/resource_update');
+        curl_setopt_array($request, [
+            CURLOPT_POST           => true,
+            CURLOPT_HEADER         => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POSTFIELDS     => $fields,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_HTTPHEADER     => ["Authorization: {$this->config['api_key']}"]
+        ]);
+        $response = curl_exec($request);
+        if ($response === false) {
+            die(curl_error($request));
+        }
+    }
+}
---
+++ crm/scripts/ckan/updateCkanResources.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @copyright 2016 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ */
+use Application\Models\Ckan;
+use Application\Models\Open311Client;
+
+include realpath(__DIR__.'/../../bootstrap.inc');
+include __DIR__.'/Ckan.php';
+$config = include realpath(__DIR__.'/site_config.inc');
+
+$ckan = new Ckan($config);
+
+foreach ($config['resource_categories'] as $resource_id=>$category_id) {
+    $file = __DIR__."/$resource_id.csv";
+
+    Open311Client::export_data($file, $category_id);
+    $ckan->upload_resource($resource_id, $file);
+}
