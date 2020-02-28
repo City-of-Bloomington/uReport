e4bf1151a75f7f18bde4948616be230634d40329
Cliff Ingham <inghamn@bloomington.in.gov>
2020-02-28 15:37:25 -0500

    Replaced jQuery ajax calls with native XMLHttpRequest
    Updates #371

--- crm/public/js/issues/changePerson.js
+++ crm/public/js/issues/changePerson.js
@@ -2,22 +2,12 @@
 var ISSUE_PERSON_CHANGER = {
 	popup: {},
 	setPerson: function (person_id) {
-        jQuery.ajax(CRM.BASE_URL + '/people/view?format=json;person_id=' + person_id, {
-            dataType: 'json',
-            success: function (person, status, xhr) {
-                document.getElementById('reportedByPerson_id').value       = person.id;
-                document.getElementById('reportedByPerson-name').innerHTML = person.fullname;
-                ISSUE_PERSON_CHANGER.popup.close();
-            }
+        CRM.ajax(CRM.BASE_URL + '/people/view?format=json;person_id=' + person_id, function (request) {
+            const person = JSON.parse(request.responseText);
+
+            document.getElementById('reportedByPerson_id').value       = person.id;
+            document.getElementById('reportedByPerson-name').innerHTML = person.fullname;
+            ISSUE_PERSON_CHANGER.popup.close();
         });
 	}
 }
-jQuery('.reportedByPerson .button').on('click', function (e) {
-    e.preventDefault();
-    ISSUE_PERSON_CHANGER.popup = window.open(
-        CRM.BASE_URL + '/people?popup=1;callback=ISSUE_PERSON_CHANGER.setPerson',
-        'popup',
-        'menubar=no,location=no,status=no,toolbar=no,width=800,height=600,resizeable=yes,scrollbars=yes'
-    );
-    return false;
-});
--- crm/public/js/tickets/searchResultsMap.js
+++ crm/public/js/tickets/searchResultsMap.js
@@ -1,6 +1,6 @@
 /**
- * @copyright 2013-2016 City of Bloomington, Indiana
- * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @copyright 2013-2020 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 'use strict';
 
@@ -31,7 +31,7 @@ google.maps.event.addDomListener(window, 'load', function () {
 			maxLng = maxLng + lngDist / 4;
 			return '[' + minLat + ',' + minLng + ' TO ' + maxLat + ',' + maxLng + ']';
 		},
-		// bbox format: xx.xxxxxx,xx.xxxxxx,xx.xxxxxx,xx.xxxxxx
+        // bbox format: xx.xxxxxx,xx.xxxxxx,xx.xxxxxx,xx.xxxxxxjson
 		generateBBox = function (bounds) {
 			var minLat = bounds.getSouthWest().lat(),
 				minLng = bounds.getSouthWest().lng(),
@@ -99,98 +99,118 @@ google.maps.event.addDomListener(window, 'load', function () {
                 ? 6
                 : 10 - (Math.floor(zoom / 2));
         },
-		refreshMap = function () {
-			var zoomLevel    = map.getZoom(),
-				bounds       = map.getBounds(),
-				bbox         = generateBBox(bounds),
-				coordinates  = generateCoordinates(bounds),
-				clusterLevel = getClusterLevel(zoomLevel),
-                url          = statsUrl(coordinates, clusterLevel),
-                updateBBox   = function (link) {
-                    var url  = link.getAttribute('href');
 
-                    url = URL.replaceParam(url, 'bbox', bbox);
-                    url = URL.replaceParam(url, 'zoom', zoomLevel);
-                    link.setAttribute('href', url);
-                };
+        /**
+         * Draws markers on the map for individual tickets
+         *
+         * @param XMLHttpRequest xhr   Request to SOLR for ticket data
+         */
+        drawTickets = function (xhr) {
+            const json    = JSON.parse(xhr.responseText),
+                  tickets = json.response.docs;
+
+            let coordinates  = {},
+                latlng       = [],
+                markerLatLng = {},
+                len          = 0,
+                i            = 0;
+
+            len = tickets.length;
+            for (i=0; i<len; i++) {
+                coordinates     = tickets[i].coordinates;
+                latlng          = coordinates.split(",");
+                markerLatLng    = new google.maps.LatLng(latlng[0],latlng[1]);
+                indivMarkers[i] = new google.maps.Marker({
+                    position: markerLatLng,
+                    map: map,
+                    title: tickets[i].id.toString()
+                });
+            }
+            len = indivMarkers.length;
+            for (i = 0; i<len; i++) {
+                oms.addMarker(indivMarkers[i]);
+            }
+        },
+
+        /**
+         * Draws cluster markers on the map
+         *
+         * @param JSON   solr          SOLR response with cluster data
+         * @param int    clusterLevel  Zoom Level to draw
+         * @param Bounds bounds        Google Map Bounds object for the map
+         * @param string coordinates   Google Map coordinate string for the bounds
+         */
+        drawClusters = function (solr, clusterLevel, bounds, coordinates) {
+            const latStats = solr.stats.stats_fields.latitude .facets['cluster_id_'+clusterLevel],
+                  lngStats = solr.stats.stats_fields.longitude.facets['cluster_id_'+clusterLevel];
+
+            let clusterId,
+                centroidLat,
+                centroidLng,
+                clusterLatLng,
+                count             = 0,
+                total             = 0,
+                largeClusterIndex = 0,
+                smallClusterIndex = 0,
+                len               = 0,
+                i                 = 0;
+
+            // Remove all the markers from the map
+            len = largeClusterIndex.length; for (i=0; i<len; i++) { largeClusters[i].setMap(null); }
+            len =      indivMarkers.length; for (i=0; i<len; i++) {  indivMarkers[i].setMap(null); }
+            largeClusters = [];
+            smallClusters = [];
+            indivMarkers  = [];
+
+            // Create markers for the clusters returned from SOLR
+            for (clusterId in latStats) {
+                count         = latStats[clusterId].count;
+                centroidLat   = latStats[clusterId].mean;
+                centroidLng   = lngStats[clusterId].mean;
+                clusterLatLng = new google.maps.LatLng(centroidLat, centroidLng);
+
+                if (bounds.contains(clusterLatLng)) { total += count; }
+
+                if (count >= 5) {
+                    largeClusters[largeClusterIndex] = new MarkerCluster(map, clusterLatLng, count);
+                    largeClusterIndex += 1;
+                }
+                else {
+                    smallClusters[smallClusterIndex] = clusterId;
+                    smallClusterIndex += 1;
+                }
+            }
+            document.getElementById('search_results_total').innerHTML = total;
+
+            // If we have any small clusters, look up the data for each individual ticket
+            if (smallClusters.length > 0) {
+                CRM.ajax(ticketsUrl(coordinates, clusterLevel, smallClusters), drawTickets);
+            }
+        },
+
+        /**
+         * Callback function for map change events
+         */
+		refreshMap = function () {
+			const zoomLevel    = map.getZoom(),
+				  bounds       = map.getBounds(),
+				  bbox         = generateBBox(bounds),
+				  coordinates  = generateCoordinates(bounds),
+				  clusterLevel = getClusterLevel(zoomLevel),
+                  url          = statsUrl(coordinates, clusterLevel),
+                  updateBBox   = function (link) {
+                      let url  = link.getAttribute('href');
+
+                      url = URL.replaceParam(url, 'bbox', bbox);
+                      url = URL.replaceParam(url, 'zoom', zoomLevel);
+                      link.setAttribute('href', url);
+                  };
 
             updateBBox(document.getElementById('apply_bbox_button'));
 
 			// Correspond with Solr Server
-            jQuery.ajax(url, {
-                dataType: 'json',
-                success: function (json, status, xhr) {
-                    var latStats = json.stats.stats_fields.latitude .facets['cluster_id_'+clusterLevel],
-                        lngStats = json.stats.stats_fields.longitude.facets['cluster_id_'+clusterLevel],
-                        clusterId,
-                        centroidLat,
-                        centroidLng,
-                        clusterLatLng,
-                        count             = 0,
-                        total             = 0,
-                        largeClusterIndex = 0,
-                        smallClusterIndex = 0,
-                        len               = 0,
-                        i                 = 0;
-
-                    // Remove all the markers from the map
-                    len = largeClusterIndex.length; for (i=0; i<len; i++) { largeClusters[i].setMap(null); }
-                    len =      indivMarkers.length; for (i=0; i<len; i++) {  indivMarkers[i].setMap(null); }
-                    largeClusters = [];
-                    smallClusters = [];
-                    indivMarkers  = [];
-
-                    // Create markers for the clusters returned from SOLR
-                    for (clusterId in latStats) {
-                        count         = latStats[clusterId].count;
-                        centroidLat   = latStats[clusterId].mean;
-                        centroidLng   = lngStats[clusterId].mean;
-                        clusterLatLng = new google.maps.LatLng(centroidLat, centroidLng);
-
-                        if (bounds.contains(clusterLatLng)) { total += count; }
-
-                        if (count >= 5) {
-                            largeClusters[largeClusterIndex] = new MarkerCluster(map, clusterLatLng, count);
-                            largeClusterIndex += 1;
-                        }
-                        else {
-                            smallClusters[smallClusterIndex] = clusterId;
-                            smallClusterIndex += 1;
-                        }
-                    }
-                    document.getElementById('search_results_total').innerHTML = total;
-
-                    // If we have any small clusters, look up the data for each individual ticket
-                    if (smallClusters.length > 0) {
-                        jQuery.ajax(ticketsUrl(coordinates, clusterLevel, smallClusters), {
-                            dataType: 'json',
-                            success: function (json, status, xhr) {
-                                var tickets      = json.response.docs,
-                                    coordinates  = {},
-                                    latlng       = [],
-                                    markerLatLng = {},
-                                    len          = 0,
-                                    i            = 0;
-
-                                len = tickets.length;
-                                for (i=0; i<len; i++) {
-                                    coordinates     = tickets[i].coordinates;
-                                    latlng          = coordinates.split(",");
-                                    markerLatLng    = new google.maps.LatLng(latlng[0],latlng[1]);
-                                    indivMarkers[i] = new google.maps.Marker({
-                                                          position: markerLatLng,
-                                                               map: map,
-                                                             title: tickets[i].id.toString()
-                                                      });
-                                }
-                                len = indivMarkers.length;
-                                for (i = 0; i<len; i++) {
-                                    oms.addMarker(indivMarkers[i]);
-                                }
-                            }
-                        });
-                    }
-                }
+            CRM.ajax(url, function (xhr) {
+                drawClusters(JSON.parse(xhr.responseText), clusterLevel, bounds, coordinates);
             });
 		};
 
