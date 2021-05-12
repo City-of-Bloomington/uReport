1924476c8967d42e19c76b81268245eb1478ef68
Cliff Ingham <inghamn@bloomington.in.gov>
2021-05-12 09:09:49 -0400

    Nightly tarballs should not include media
    We will rely on external backups systems to capture all the user uploaded images.
    
    Updates #387

--- ansible/templates/backup.sh
+++ ansible/templates/backup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # Creates a tarball containing a full snapshot of the data in the site
 #
-# @copyright 2011-2017 City of Bloomington, Indiana
-# @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.txt
+# @copyright 2011-2021 City of Bloomington, Indiana
+# @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE
 
 # Where to store the nightly backup tarballs
 APPLICATION_NAME="ureport"
-MYSQLDUMP=`which mysqldump`
 MYSQL_DBNAME="{{ ureport_db.name }}"
 MYSQL_CREDENTIALS="/etc/cron.daily/backup.d/${APPLICATION_NAME}.cnf"
 BACKUP_DIR="{{ ureport_backup_path }}"
@@ -23,15 +22,11 @@ now=`date +%s`
 today=`date +%F`
 
 # Dump the database
-$MYSQLDUMP --defaults-extra-file=$MYSQL_CREDENTIALS $MYSQL_DBNAME > $SITE_HOME/$MYSQL_DBNAME.sql
-
-# Tarball the Data
-cd ${SITE_HOME}/..
-data=${SITE_HOME##*/}
-tar czf $BACKUP_DIR/$today.tar.gz $data
+cd $BACKUP_DIR
+mysqldump --defaults-extra-file=$MYSQL_CREDENTIALS $MYSQL_DBNAME > $today.sql
+gzip $today.sql
 
 # Purge any backup tarballs that are too old
-cd $BACKUP_DIR
 for file in `ls`
 do
 	atime=`stat -c %Y $file`
