89d11df0ad44a8c590438f983235e2c6f52d242a
Cliff Ingham <inghamn@bloomington.in.gov>
2025-03-07 10:22:49 -0500

    Graylog writer no longer depends on laminas
    Updates #410

--- crm/src/Application/GraylogWriter.php
+++ crm/src/Application/GraylogWriter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2020 City of Bloomington, Indiana
+ * @copyright 2020-2025 City of Bloomington, Indiana
  * @license https://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 declare (strict_types=1);
@@ -10,36 +10,56 @@ use Gelf\Publisher;
 use Gelf\Message;
 use Gelf\Transport\UdpTransport;
 
-use Laminas\Log\Writer\AbstractWriter;
-
 class GraylogWriter extends AbstractWriter
 {
-    private $publisher;
-
-    public function __construct(string $url, int $port)
+    public static function doWrite(array $event)
     {
-        $this->url = $url;
+        $transport = new UdpTransport(GRAYLOG_DOMAIN, GRAYLOG_PORT, UdpTransport::CHUNK_SIZE_LAN);
+        $publisher = new Publisher();
+        $publisher->addTransport($transport);
 
-        $transport = new UdpTransport($url, $port, UdpTransport::CHUNK_SIZE_LAN);
-        $this->publisher = new Publisher();
-        $this->publisher->addTransport($transport);
-    }
-
-    public function doWrite(array $event)
-    {
         $message = new Message();
         if (!empty($event['message'])) { $message->setShortMessage($event['message']); }
         if (!empty($event['errno'  ])) { $message->setLevel       ($event['errno'  ]); }
         if (!empty($event['file'   ])) { $message->setFile        ($event['file'   ]); }
         if (!empty($event['line'   ])) { $message->setLine        ($event['line'   ]); }
 
-        if (!empty($event['priority'     ])) { $message->setLevel($event['priority']); }
-        if (!empty($event['extra']['file'])) { $message->setFile ($event['extra']['file']); }
-        if (!empty($event['extra']['line'])) { $message->setLine ($event['extra']['line']); }
         $message->setAdditional('base_uri', BASE_URI);
-        $message->setAdditional('request_uri', $_SERVER['REQUEST_URI'] ?? 'command line');
+        if (!empty($_SERVER['REQUEST_URI'])) {
+            $message->setAdditional('request_uri', $_SERVER['REQUEST_URI']);
+        }
         $message->setFullMessage(print_r($event, true));
 
-        $this->publisher->publish($message);
+        $publisher->publish($message);
+    }
+
+    public static function error(int $error, string $message, string $file, int $line)
+    {
+        $e = [
+            'errno'   => $error,
+            'message' => $message,
+            'file'    => $file,
+            'line'    => $line
+        ];
+        self::doWrite($e);
+    }
+
+    public static function exception($e)
+    {
+        $e = [
+            'errno'   => $e->getCode(),
+            'message' => $e->getMessage(),
+            'file'    => $e->getFile(),
+            'line'    => $e->getLine(),
+            'trace'   => $e->getTrace()
+        ];
+        self::doWrite($e);
+
+    }
+
+    public static function shutdown()
+    {
+        $e = error_get_last();
+        if ($e) { self::doWrite($e); }
     }
 }
