d027b22e7afdfd7635b5a620713bd177e7c9c426
Cliff Ingham <inghamn@bloomington.in.gov>
2025-03-07 10:17:18 -0500

    Removed Auth0 support
    We never implemented public user accounts via Auth0.  Public user accounts will be generic OIDC, in the future.
    
    Updates #4

--- crm/src/Application/Controllers/LoginController.php
+++ crm/src/Application/Controllers/LoginController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2012-2016 City of Bloomington, Indiana
+ * @copyright 2012-2025 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 namespace Application\Controllers;
@@ -36,34 +36,6 @@ class LoginController extends Controller
             // and the user's login name can be read with phpCAS::getUser().
 
             $this->registerUser(\phpCAS::getUser());
-        }
-		header('Location: '.BASE_URL.'/login?return_url='.$this->return_url);
-		exit();
-	}
-
-	/**
-	 * Authenticate users against Auth0.com
-	 */
-	public function auth0()
-	{
-        global $AUTH0;
-
-        if (isset($AUTH0)) {
-            $auth0 = new Auth0([
-                'domain'        => $AUTH0['domain'       ],
-                'client_id'     => $AUTH0['client_id'    ],
-                'client_secret' => $AUTH0['client_secret'],
-                'redirect_uri'  => BASE_URL.'/login/auth0',
-                'audience'      => "https://$AUTH0[domain]/userinfo",
-                'scope'         => 'openid profile'
-            ]);
-            $user = $auth0->getUser();
-            if ($user) {
-                $this->registerUser($user['name']);
-            }
-            else {
-                $auth0->login();
-            }
         }
 		header('Location: '.BASE_URL.'/login?return_url='.$this->return_url);
 		exit();
